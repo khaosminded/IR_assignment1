526c78b55501a2837b8d08757c2367b4279467fd
hanxinlei <khaosminded@vip.qq.com>
2017-10-15 17:50:18 -0700

    almost

--- a.py
+++ a.py
@@ -4,10 +4,12 @@
 #by_xinlei_han
 import os
 import re
+import math
 
- #receive a string f
+#receive a string f
 def count(f):
-
+    #lower
+    f=f.lower()
     #count tokens
     token_count=0
     token_list=[',','.','/','?','\'','"',':',';','(',')','!','-']
@@ -15,9 +17,9 @@ def count(f):
         token_count+=f.count(token)
     #string => list    
     text=[]
-    lines=f.split('\n')
+    lines=f.strip().split('\n')
     for line in lines:
-        text.extend(line.strip().split(' '))
+        text.extend(re.sub(r'[,\./?\'":;()!\-\s]+',' ',line).strip().split())
     #count unique terms
     unique_term=[]
     tf={}
@@ -29,11 +31,62 @@ def count(f):
             tf[text[i]]=text.count(text[i])
     return([token_count,tf])
 
-
+#assignment1 request         
+token_num=0
+unique_num=0
+val_eq1_num=0
+val_top30=[]
+docs_num=0
+#main
+results=[]
 file_list = os.listdir(os.getcwd())
-#for file_name in file_list:
-#    print(file_name)
-with open(file_list[1],'r') as f:
-    result=count(f.read().decode("utf-8-sig").encode("utf-8"))
-#print(sorted(tf.iteritems(),key=lambda d:d[1],reverse=True))
-    print(result[0])
+for file_name in file_list:
+    docs_num+=1
+    with open(file_list[1],'r') as f:
+        #results is a list in form of[[token_count,tf],..]
+        results.append(count(f.read().decode("utf-8-sig").encode("utf-8")))
+
+#calculate answer of request
+aggregate={}
+for i in results:
+    #1
+    token_num+=i[0]
+for i in results:
+    for (k,v) in i[1].items():
+        if aggregate.get(k, -1) == -1:
+           aggregate[k]=v
+        else:
+           aggregate[k]+=v 
+
+#2
+unique_num=len(aggregate)
+Nt=0#sum all terms up
+for (k,v) in aggregate.items():
+        Nt+=v
+        if v==1:
+            #3
+            val_eq1_num+=1
+list_top30=sorted(aggregate.iteritems(),key=lambda d:d[1],reverse=True)[0:30]
+
+#4
+for (k,v) in list_top30:
+    #val_top30[term,TF,IDF,TF*IDF,P]
+    DFt=0
+    print k
+    for obj in results:#each obj of result =>a file. obj[1] unique words dict.
+        if obj[1].get(k,-1) != -1:
+            DFt+=1
+    IDF=math.log(10,docs_num*1.0/DFt)
+    val_top30.append(list(k,v,IDF,IDF*v,v*1.0/Nt))
+
+#5
+avg_t=token_num*1.0/docs_num
+#print token_num,unique_num,val_eq1_num,avg_t
+#print '*******TOP30********\n%s' % str(val_top30)
+
+
+#main end
+#math.log()
+#print aggregate.get('',-1)
+#with open ('/Users/hanxinlei/wkp/log','w') as f:
+#    f.write(str(results))
